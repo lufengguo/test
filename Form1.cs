6e8c31b40e620a9d7cb67f9eee17b1db6f621484
自由飞翔 <727155007@qq.com>
2021-11-23 14:59:10 +0800

    2222

--- Form1.cs
+++ Form1.cs
@@ -14,7 +14,7 @@ namespace WindowsFormsApp1
     {
         public Form1()
         {
-            InitializeComponent();ss
+            InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)

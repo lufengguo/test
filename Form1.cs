b643ce8ae83a424b4b6723035a5111d056f016bc
自由飞翔 <727155007@qq.com>
2021-11-23 17:05:24 +0800

    从

--- Form1.cs
+++ Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class Fordweaewrm1 : Form
     {
         public Form1()
         {
